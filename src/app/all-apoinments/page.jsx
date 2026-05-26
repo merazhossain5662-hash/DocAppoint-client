6d69835a7557182f9301d8580786c96b579357ad
merazhossain5662-hash <merazhossain5662@gmail.com>
2026-05-26 18:26:07 +0600

    removed console log token and added link in read me file

--- src/app/all-apoinments/page.jsx
+++ src/app/all-apoinments/page.jsx
@@ -11,10 +11,6 @@ export async function generateMetadata() {
 }
 
 export default async function page(){
-  const {token} = await auth.api.getToken({
-              headers: await headers() 
-          });
-          console.log(token);
           
   return <AllAppointment></AllAppointment>
 }
